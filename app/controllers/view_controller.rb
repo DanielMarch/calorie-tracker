7305a28141e4f5fe4ee0201207a546100da22aa2
Seth March <seth.d.march@gmail.com>
2014-08-12 13:34:57 -0500

    MVP reached

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -1,5 +1,6 @@
 class ViewController
   def view
-    puts "Your calorie count for the day is :"
+    sum = Environment.database.execute("SELECT SUM(calories) FROM event WHERE date LIKE '#{DateTime.now.strftime("%y%m%d")}'").last
+    puts "Your total calorie count for the day is #{sum[0]}"
   end
 end
